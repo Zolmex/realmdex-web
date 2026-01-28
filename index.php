c7c06298f98c5a2b570993a5cff29ec155d5b2c5
Zolmex <zolmexyt@gmail.com>
2026-01-28 12:18:52 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/index.css">
+    <title>RealmDex PServer Stats</title>
+    <link rel="icon" type="image/x-icon" href="/favicon.ico">
+    <script src="https://code.jquery.com/jquery-4.0.0.js" integrity="sha256-9fsHeVnKBvqh3FB2HYu7g2xseAZ5MlN6Kz/qnkASV8U=" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <header class="site-header" role="banner">
+        
+    </header>
+    <main class="main-content" role="main">
+
+    </main>
+    <script type="module" src="scripts/index.js"></script>
+</body>
+
+</html>
